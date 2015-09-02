6415a6473c94f7eb156709a213fa3e8b7d7531fa
zhuyuanbin <zhu.yuanbin@gmail.com>
2015-09-02 16:13:15 +0800

    test

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ ip=""
 username=""
 passwd=""
 
+result=""
+
 child=pexpect.spawn("telnet %s" % ip)
 index=child.expect(["[uU]sername:",pexpect.EOF,pexpect.TIMEOUT])
 if index==0:
@@ -17,14 +19,15 @@ if index==0:
         child.sendline("show vlan-smart-qinq")
         child.expect("show vlan-smart-qinq")
         while True:
-            index=child.expect(["--More--",".*#",pexpect.EOF,pexpect.TIMEOUT])
+            index=child.expect(["--More--","#",pexpect.EOF,pexpect.TIMEOUT])
             if index==0:
                 print child.before
+                result += child.before
                 child.sendline(" ")
             elif index==1:
-                print "last"
                 print child.before
                 print "success"
+                result += child.before
                 child.close(force=True)
                 break
             else:
@@ -35,3 +38,5 @@ if index==0:
         child.close(force=True)
 else:
     child.close(force=True)
+
+print result
