e323603f61c659aed3c87748e6348cc22d46853d
sjava <sjava@163.com>
2015-09-03 18:13:10 +0800

    add function

--- test.py
+++ test.py
@@ -5,49 +5,61 @@ import re
 from itertools import groupby
 from operator import itemgetter
 
-ip=""
-username=""
-passwd=""
+ip = ""
+username = ""
+passwd = ""
 
-result=""
+result = ""
 
-child=pexpect.spawn("telnet %s" % ip)
-index=child.expect(["[uU]sername:",pexpect.EOF,pexpect.TIMEOUT])
-if index==0:
-    child.sendline(username)
-    index=child.expect(["[pP]assword:",pexpect.EOF,pexpect.TIMEOUT])
-    child.sendline(passwd)
-    index=child.expect([".*#",pexpect.EOF,pexpect.TIMEOUT])
-    if index==0:
-        child.sendline("show vlan-smart-qinq")
-        child.expect("show vlan-smart-qinq")
-        while True:
-            index=child.expect(["--More--","#",pexpect.EOF,pexpect.TIMEOUT])
-            if index==0:
-                result += child.before
-                child.sendline(" ")
-            elif index==1:
-                result += child.before
-                child.close(force=True)
-                break
-            else:
-                child.close(force=True)
-                break
+
+def zte(ip, username="", passwd="", filename="result.txt"):
+    """TODO: Docstring for zte.
+
+    :ip: TODO
+    :username: TODO
+    :passwd: TODO
+    :filename: TODO
+    :returns: TODO
+
+    """
+    child = pexpect.spawn("telnet %s" % ip)
+    index = child.expect(["[uU]sername:", pexpect.EOF, pexpect.TIMEOUT])
+    if index == 0:
+        child.sendline(username)
+        index = child.expect(["[pP]assword:", pexpect.EOF, pexpect.TIMEOUT])
+        child.sendline(passwd)
+        index = child.expect([".*#", pexpect.EOF, pexpect.TIMEOUT])
+        if index == 0:
+            child.sendline("show vlan-smart-qinq")
+            child.expect("show vlan-smart-qinq")
+            while True:
+                index = child.expect(["--More--", "#", pexpect.EOF, pexpect.TIMEOUT])
+                if index == 0:
+                    result += child.before
+                    child.sendline(" ")
+                elif index == 1:
+                    result += child.before
+                    child.close(force=True)
+                    break
+                else:
+                    child.close(force=True)
+                    break
+        else:
+            child.close(force=True)
     else:
         child.close(force=True)
-else:
-    child.close(force=True)
 
+    temp = result.split('\r\n')
+    lrst = [x.strip(' \x08') for x in temp if x.strip(' \x08').startswith('epon')]
+    lrst = [re.split('\s+', x) for x in lrst]
+    lrst = sorted(lrst, key=lambda x: int(x[5]))
 
-temp=result.split('\r\n')
-lrst=[x.strip(' \x08') for x in temp if x.strip(' \x08').startswith('epon')]
-lrst=[re.split('\s+',x) for x in lrst]
-lrst=sorted(lrst,key=lambda x:int(x[5]))
+    for key, items in groupby(lrst, itemgetter(5)):
+        items = list(items)
+        if len(items) > 1:
+            print key
+            for i in items:
+                print i
+            print "-" * 20
 
-for key, items in groupby(lrst,itemgetter(5)):
-    items=list(items)
-    if len(items)>1:
-        print key
-        for i in items:
-            print i
-        print "-"*20
+zte(ip, username, passwd)
