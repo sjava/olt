f000752358f96aadb14ade18d5dce7c8dba201d8
sjava <sjava@163.com>
2015-09-28 23:51:11 -0700

    test

--- sw/switch.py
+++ sw/switch.py
@@ -10,8 +10,30 @@ passwd = config.get('switch', 'passwd')
 super_passwd = config.get('switch', 'super_passwd')
 
 
-def telnet_sw(ip, username=username, passwd=passwd, super_passwd=super_passwd):
+def telnet_s93(ip, username=username, passwd=passwd, super_passwd=super_passwd):
     child = pexpect.spawn('telnet {0}'.format(ip))
+    fout = file('out.log', 'w')
+    child.logfile = fout
+
+    index = child.expect(['Username:', pexpect.EOF, pexpect.TIMEOUT])
+    if index != 0:
+        child.close(force=True)
+        return None
+    child.sendline(username)
+
+    index = child.expect(['Password:', pexpect.EOF, pexpect.TIMEOUT])
+    if index != 0:
+        child.close(force=True)
+        return None
+    child.sendline(passwd)
+    child.expect('>')
+    child.sendline('super')
+
+    index = child.expect(['Password:', pexpect.EOF, pexpect.TIMEOUT])
+    if index != 0:
+        child.close(force=True)
+        return None
+    child.sendline(super_passwd)
 
 
 def main():
