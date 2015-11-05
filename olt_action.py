7278dba2f32052289e9157bf24c03ac73d37a2c5
sjava <sjava@163.com>
2015-11-05 11:25:20 +0800

    test

---
+++ olt_action.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import device.olt
+import configparser
+import os
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+zte_olt_username = config.get('olt', 'zte_username')
+zte_olt_password = config.get('olt', 'zte_password')
+
+hw_olt_username = config.get('olt', 'hw_username')
+hw_olt_username = config.get('olt', 'hw_password')
+
+olts_file, log_file, result_file = ('olts.txt', 'result/olt_log.txt',
+                                    'result/olt_info.txt')
+zte_command = "show monitor session 1"
+hw_command = ""
+
+
+def olt_check():
+    for f in [log_file, result_file]:
+        if os.path.exists(f):
+            os.remove(f)
+        os.mknod(f)
+
+    with open(olts_file) as olts, open(log_file, 'a') as flog, open(
+            result_file, 'a') as fresult:
+        for olt in olts:
+            mark = 'fail'
+            record = []
+            ip, factory, area = [x.strip() for x in olt.split(',')]
+            if factory.lower() == 'zte':
+                mark, record = device.olt.zte_get_info(
+                    ip, zte_olt_username, zte_olt_password, zte_command)
+            elif factory.lower() == 'hw':
+                pass
+            flog.write('{0}:{1}\n'.format(olt.strip(), mark))
+            record = record_clear(record)
+            if record and mark == 'success':
+                fresult.write('{0}:\n'.format(olt.strip(), ))
+                for r in record:
+                    fresult.write('{0}\n'.format(r, ))
+                fresult.write('*' * 60 + '\n')
+
+
+def record_clear(record):
+    if record:
+        record = [x for x in record if 'Session 1' in x]
+    return record
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
