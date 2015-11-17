a7a4f9e631342837424756400bf4562dd5909598
sjava <sjava@163.com>
2015-11-17 20:33:47 +0800

    test

--- olt_cards.py
+++ olt_cards.py
@@ -17,8 +17,8 @@ neo4j_password = config.get('neo4j', 'password')
 olts_file, log_file, result_file = ('olts.txt', 'result/olt_log.txt',
                                     'result/olt_info.txt')
 
-# authenticate('61.155.48.36:7474', neo4j_username, neo4j_password)
-# graph = Graph("http://61.155.48.36:7474/db/data")
+authenticate('61.155.48.36:7474', neo4j_username, neo4j_password)
+graph = Graph("http://61.155.48.36:7474/db/data")
 
 
 def clear_log():
@@ -35,42 +35,38 @@ def get_cards(olt):
     return functions.get(company, no_company)(ip) + [','.join(olt)]
 
 
-def create_card_node(card):
-    node, = graph.create(Node('Card', slot=card[0], name=card[1]))
-    return node
+#  def create_card_node(card):
+    #  node, = graph.create(Node('Card', slot=card[0], name=card[1]))
+    #  return node
 
 
 def output_info(info):
-    authenticate('61.155.48.36:7474', neo4j_username, neo4j_password)
-    graph = Graph("http://61.155.48.36:7474/db/data")
+    create_card_node = lambda x: graph.create(Node('Card', slot=x[0], name=x[1]))[0]
     mark, result, olt = info
     with open(log_file, 'a') as logging:
         logging.write("{0}:{1}\n".format(olt, mark))
     if result and mark == 'success':
         ip = olt.split(',')[0]
         node = graph.find_one('Olt', property_key='ip', property_value=ip)
-        card_nodes = list(map(create_card_node, result))
+        card_nodes = map(create_card_node, result)
         list(map(lambda x: graph.create((node, 'HAS', x)), card_nodes))
 
 
 def import_cards():
     clear_log()
-    nodes = graph.find('Olt', property_key='ip', property_value='222.188.51.211')
-    #  nodes = graph.find('Olt', property_key='company', property_value='zte')
+    #  nodes = graph.find('Olt', property_key='ip', property_value='222.188.51.211')
+    nodes = graph.find('Olt', property_key='company', property_value='zte')
     # ip = [x.strip('\r\n') for x in open('ip.csv')]
     # nodes = [graph.find_one('Olt',
     #                         property_key='ip',
     #                         property_value=x) for x in ip]
     #  nodes = graph.find('Olt', property_key='ip', property_value='61.147.63.247')
     olts = [(x['ip'], x['company'], x['area']) for x in nodes]
-    pool = Pool(4)
-    list(pool.map(compose(output_info, get_cards), olts))
-    pool.close()
-    pool.join
+    list(map(compose(output_info, get_cards), olts))
 
 
 def main():
-    pass
+    import_cards()
 
 
 if __name__ == '__main__':
